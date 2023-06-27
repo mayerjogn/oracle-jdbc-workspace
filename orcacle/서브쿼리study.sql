343c13b5b4078ea38d39012f196f47d70430b885
yejin1219 <130232731+mayerjogn@users.noreply.github.com>
2023-06-27 17:46:50 +0900

    과제 3번 9,12,14,15,16,17,18

--- orcacle/서브쿼리study.sql
+++ orcacle/서브쿼리study.sql
@@ -16,9 +16,15 @@ WHERE ROWNUM < 6;
 
 -- 02 부서별 급여 합계가 전체 급여 총 합의 20%보다 많은 부서의 부서명, 부서별 급여 합계 조회
 --(방법은 여러 가지..! 가급적으로 서브쿼리 사용해보세요!)
+
+
 SELECT DEPT_TITLE, SUM(SALARY)
 FROM EMPLOYEE
-JOIN DEPARTMENT ON ()
+LEFT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+GROUP BY DEPT_CODE, DEPT_TITLE
+HAVING SUM(SALARY) > 0.2* (SELECT (SUM(SALARY))
+FROM EMPLOYEE);
+--GROUP BY DEPT_CODE);
 
 
 -- 03 WITH을 이용하여 급여 합과 급여 평균 조회
