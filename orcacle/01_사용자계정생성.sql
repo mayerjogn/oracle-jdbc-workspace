2b8a74e1881da5adf28f401a5ef115c27f94472a
mayerjogn <130232731+mayerjogn@users.noreply.github.com>
2023-10-06 19:22:40 +0900

    ㅇㅁㄴ

--- orcacle/01_사용자계정생성.sql
+++ orcacle/01_사용자계정생성.sql
@@ -1,7 +1,10 @@
 -- 사용자 계정 생성하는 구문 : 관리자 계정만이 할 수 있는 역할
 -- [표현법] CREATE USER 계정명 IDENTIFIED BY 비밀번호;
 CREATE USER kh IDENTIFIED BY kh;
-
+DROP USER team4 cascade;
+CREATE USER TEAM4 IDENTIFIED BY TEAM4;
+GRANT RESOURCE, CONNECT TO TEAM4;
+GRANT UNLIMITED TABLESPACE TO TEAM4;
 -- 위에서 만들어진 사용자 계정에게 최소한의 권한(데이터관리, 접속) 부여
 -- [표현법] GRANT 권한 1, 권한 2, ... TO 계정명;
 -- RESOURCE는 객체(생성, 수정, 삭제), 데이터(입력, 수정, 조회, 삭제) 권한 
